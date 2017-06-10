ff81dd10d747829203c1264b56a2e71b83eb7fbe
Brijesh <Brijesh@Brijeshs-MacBook-Pro.local>
2017-06-10 15:34:39 +0530

    Adding left,right and cornerradius functionality

--- SwiftyTextField/Classes/SwiftyTextField.swift
+++ SwiftyTextField/Classes/SwiftyTextField.swift
@@ -2,24 +2,71 @@
 //  SwiftyTextField.swift
 //  Pods
 //
-//  Created by Brijesh on 10/06/17.
+//  Created by Shiroya&Bros on 10/06/17.
 //
 //
-
 import Foundation
 import UIKit
-
-class SwiftyTextField: UITextField {
+@IBDesignable
+class SwiftyTextField:UITextField{
+    //MARK: - Variables -
+    var _leftPadding:CGFloat = 0.0
+    var _rightPadding:CGFloat = 0.0
     
     //MARK: - Initialization -
-    override public init(frame: CGRect) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
-     }
+    }
     
-    required public init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    override public func awakeFromNib() {
+    //MARK: - IBInspactable -
+    /*
+     This property set corner radius of textfield .
+     If user not give any valur to this property then default valur is 0.0 .If user set value then apply on it.
+     */
+    @IBInspectable var CornerRadius:CGFloat = 0.0{
+        didSet{
+            self.layer.cornerRadius = CornerRadius
+            self.layer.masksToBounds = true
+        }
+    }
+    
+    
+    /*
+     LeftPadding set left space of textfield that means front space is set and when user start writing into textfield, writing is start after leftspace into textfield.
+     */
+    @IBInspectable var LeftPadding:CGFloat = 0.0{
+        didSet{
+            self._leftPadding = LeftPadding
+        }
+    }
+    
+    /*
+     RightPadding set right space of textfield that means rear space is set and when user start writing into textfield, writing is start after rightspace into textfield.
+     */
+    @IBInspectable var RightPadding:CGFloat = 0.0{
+        didSet{
+            self._rightPadding = RightPadding
+        }
+    }
+    
+    
+    /*
+     set left,right,bottom and top space into textfield. Set four side space when user editing or after editing or before editing.
+     */
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, UIEdgeInsets.init(top: 0, left: self._leftPadding, bottom: 0.0, right: self._rightPadding))
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, UIEdgeInsets.init(top: 0, left: self._leftPadding, bottom: 0.0, right: self._rightPadding))
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, UIEdgeInsets.init(top: 0, left: self._leftPadding, bottom: 0.0, right: self._rightPadding))
     }
 }
+
